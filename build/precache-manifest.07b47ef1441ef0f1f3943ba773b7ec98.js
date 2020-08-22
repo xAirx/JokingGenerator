d5b1b89c7d479b2d4d5db200907f8defc255f5af
xAirx <marcowurtz@hotmail.com>
2020-08-22 19:06:48 +0200

    up
    up

---
+++ build/precache-manifest.07b47ef1441ef0f1f3943ba773b7ec98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fda783a2edf1c874cd995f15c7249a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a3cdbbf503c3bb6298c",
+    "url": "/static/css/main.970b55c9.chunk.css"
+  },
+  {
+    "revision": "fa8368c28c4457773e1b",
+    "url": "/static/js/2.5ee416ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ee416ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3cdbbf503c3bb6298c",
+    "url": "/static/js/main.e5af006f.chunk.js"
+  },
+  {
+    "revision": "de32e627726085ee3b62",
+    "url": "/static/js/runtime-main.f08f17fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
